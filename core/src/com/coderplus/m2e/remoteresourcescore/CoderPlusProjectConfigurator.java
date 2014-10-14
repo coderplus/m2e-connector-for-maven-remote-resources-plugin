5112db09b8f463a2468209a00e033a07b4a99198
Aneesh Joseph <admin@coderplus.com>
2014-10-14 11:39:06 +0530

    updated so that the resources are added to eclipse sources(if required)

--- core/src/com/coderplus/m2e/remoteresourcescore/CoderPlusProjectConfigurator.java
+++ core/src/com/coderplus/m2e/remoteresourcescore/CoderPlusProjectConfigurator.java
@@ -10,28 +10,67 @@
  *******************************************************************************/
 package com.coderplus.m2e.remoteresourcescore;
 
+import java.io.File;
+
 import org.apache.maven.plugin.MojoExecution;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.MavenProjectUtils;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
-import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
 import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
+import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
+import org.eclipse.m2e.jdt.IClasspathDescriptor;
+import org.eclipse.m2e.jdt.IClasspathEntryDescriptor;
 
-public class CoderPlusProjectConfigurator extends AbstractProjectConfigurator {
+@SuppressWarnings({ "deprecation" })
+public class CoderPlusProjectConfigurator extends AbstractJavaProjectConfigurator {
+
+	private static final String OUTPUT_DIRECTORY = "outputDirectory";
+	private static final String ATTACHED_TO_TEST = "attachedToTest";
+	private static final String ATTACHED_TO_MAIN = "attachedToMain";
+	private static final String ATTACHED = "attached";
 
 	@Override
-	public AbstractBuildParticipant getBuildParticipant(IMavenProjectFacade projectFacade, MojoExecution execution,
-			IPluginExecutionMetadata executionMetadata) {
-
+	public AbstractBuildParticipant getBuildParticipant(IMavenProjectFacade projectFacade, MojoExecution execution,IPluginExecutionMetadata executionMetadata) {
 		return new CoderPlusBuildParticipant(execution);
 	}
 
 	@Override
-	public void configure(ProjectConfigurationRequest projectConfigurationRequest, IProgressMonitor iProgressMonitor)
+	public void configureRawClasspath(ProjectConfigurationRequest request,IClasspathDescriptor classpath, IProgressMonitor monitor)
 			throws CoreException {
-		// Nothing to configure
+		IMavenProjectFacade facade = request.getMavenProjectFacade();
+		MavenProject mavenProject = facade.getMavenProject();
+		IProject project = facade.getProject();
+		assertHasNature(request.getProject(), "org.eclipse.jdt.core.javanature");
+		for (MojoExecution execution : getMojoExecutions(request, monitor)) {
+			boolean attached = Boolean.TRUE.equals(maven.getMojoParameterValue(mavenProject, execution, ATTACHED,Boolean.class, new NullProgressMonitor()));
+			boolean attachedToMain = Boolean.TRUE.equals(maven.getMojoParameterValue(mavenProject, execution, ATTACHED_TO_MAIN,Boolean.class, new NullProgressMonitor()));
+			boolean attachedToTest = Boolean.TRUE.equals(maven.getMojoParameterValue(mavenProject, execution, ATTACHED_TO_TEST,Boolean.class, new NullProgressMonitor()));
+			File outputDirectory = maven.getMojoParameterValue(mavenProject, execution, OUTPUT_DIRECTORY,File.class, new NullProgressMonitor());
+			IClasspathEntryDescriptor descriptor = null;
+			if(attached || attachedToMain){
+				IPath relativeSourcePath = MavenProjectUtils.getProjectRelativePath(project,outputDirectory.getAbsolutePath());
+				descriptor  = classpath.addSourceEntry(project.getFullPath().append(relativeSourcePath),facade.getOutputLocation(), true);
+			} else  if(attachedToTest){
+				IPath relativeSourcePath = MavenProjectUtils.getProjectRelativePath(project,outputDirectory.getAbsolutePath());
+				descriptor = classpath.addSourceEntry(project.getFullPath().append(relativeSourcePath),facade.getTestOutputLocation(), true);
+			}
+
+			if(descriptor!= null){
+				IPath[] paramArrayOfIPath = new IPath[1];
+				paramArrayOfIPath[0] = new Path("**");
+				descriptor.setExclusionPatterns(paramArrayOfIPath);
+			}
+
+		}
 	}
+
 
 }
